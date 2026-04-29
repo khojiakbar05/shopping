b3314d70be7abea12d46e31d2f6d86e3907a9412
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-04-29 09:14:13 +0900

    feat: building simple backend server on NodeJS

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 // NodeJs  EXPRESS NESTJS
 // Python DJANGO FastAPI
+
+const express = require('express')
+const app = express()
+
+// Traditional get API
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+
+// Rest API  
+app.get('/rest', (req, res) => {
+  res.json({name: "Marco", age: 21, group: "MIT"})
+})
+
+app.listen(3000, () => {
+  console.log(`Backend server is running on port 3000`)
+})
